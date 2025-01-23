3b8357c21b0c5ba86e1dc4bdfe192b51149f9aa7
Angel <angelin09ozoz@gmail.com>
2025-01-22 22:53:47 -0600

    first commit

--- src/test/java/com/angel/springcloud/msvc/usuarios/MsvcUsuariosApplicationTests.java
+++ src/test/java/com/angel/springcloud/msvc/usuarios/MsvcUsuariosApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MsvcUsuariosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
