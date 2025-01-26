39988b16970768f18b955cc1f7af539bcf5d0e81
Angel <angelin09ozoz@gmail.com>
2025-01-25 18:16:19 -0600

    Agregando ejemplos de respuestas HTTP en GET por ID

---
+++ src/main/java/com/angel/springcloud/msvc/usuarios/controllers/examples/UsuarioExamples.java
@@ -0,0 +1,6 @@
+package com.angel.springcloud.msvc.usuarios.controllers.examples;
+
+public class UsuarioExamples {
+    public static final String USUARIO_OBTENIDO = "{ \"id\": 1, \"nombre\": \"Juan Pérez\", \"email\": \"juan.perez@example.com\" }";
+    public static final String USUARIO_NO_ENCONTRADO = "{ \"mensaje\": \"El usuario no existe\" }";
+}
